9f45bfc2530c4abb10d91204770385da796a854a
abhijith subash <abhijithsubash07@gmail.com>
2024-08-29 23:05:25 +0530

    fixed: styles in about page. added skills button for styling

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,26 @@ import styles from "./About.styles";
 import DownArrow from '../../components/DownArrow/DownArrow';
 
 function About({aboutRef, projectsRef}) {
+    // <div className="qualification qualification-1">
+//     <div className="qualification-name"></div>
+//     <div className="qualification-institution"></div>
+//     <div className="qualification-year">2021-2023</div>
+// </div>
+    const skills = ['Python','JavaScript','C','Java']
+    const qualifications = [
+        {
+            name: "M.Sc. Computer Science",
+            institution: "DCS, Kariavattom Campus",
+            startYear: 2021,
+            endYear: 2023,
+        },
+        {
+            name: "B.Sc. Computer Science",
+            institution: "EMSCAS, Iritty",
+            startYear: 2018,
+            endYear: 2021,
+        },
+    ]
     return (
         <>
         <styles.Wrapper ref={aboutRef}>
@@ -9,6 +29,7 @@ function About({aboutRef, projectsRef}) {
                 <styles.Certificates>
                     <div className="span-heading certificate-heading">
                         <span>My certificates</span>
+                        <img src="images/arrows/certificates.svg" alt="" />
                     </div>
                     <div className="certificate-container">
                         <i className="left-arrow">&lt;</i>
@@ -16,13 +37,14 @@ function About({aboutRef, projectsRef}) {
                         <i className="right-arrow">&gt;</i>
                     </div>
                 </styles.Certificates>
-                <div className="skills">
-                    <div className="btn-tech-skills"></div>
-                    <div className="tech-skill skill-1"></div>
-                    <div className="tech-skill skill-2"></div>
-                    <div className="tech-skill skill-3"></div>
-                    <div className="tech-skill skill-4"></div>
-                </div>
+                <styles.SkillLists>
+                    <button>My TECH <span>Skills</span></button>
+                    <div className="tech-skill">
+                    {skills.map((skill, index) => (
+                        <div key={index} className={`skill-${index}`}>{skill}</div>
+                    ))}
+                    </div>
+                </styles.SkillLists>
             </styles.SkillContainer>
             <styles.ImageContainer>
                 <img src="/images/abhijith_image.jpg" alt="" />
@@ -34,16 +56,16 @@ function About({aboutRef, projectsRef}) {
                     <img src="images/arrows/qualifications.svg" alt="" />
                 </div>
                 <div className="qualifications">
-                    <div className="qualification qualification-1">
-                        <div className="qualification-name">M.Sc. Computer Science</div>
-                        <div className="qualification-institution">DCS, Kariavattom Campus</div>
-                        <div className="qualification-year">2021-2023</div>
-                    </div>
-                    <div className="qualification qualification-2">
-                        <div className="qualification-name">B.Sc. Computer Science</div>
-                        <div className="qualification-institution">EMSCAS, Iritty</div>
-                        <div className="qualification-year">2018-2021</div>
-                    </div>
+                    {
+                        qualifications.map((qualification, index) =>(
+                            <div className={`qualification qualification-${index}`} key={index}>
+                                <div className="qualification-name">{qualification.name}</div>
+                                <div className="qualification-institution">{qualification.institution}</div>
+                                <div className="qualification-year">{qualification.startYear}-{qualification.endYear}</div>
+                            </div>
+                        ))
+                    }
+                    
                 </div>
                 <div className="interests">
                     <p className="span-heading interest-heading">My interests</p>
--- src/pages/About/About.styles.js
+++ src/pages/About/About.styles.js
@@ -3,7 +3,7 @@ import styled, {css} from "styled-components"
 const spanHeading = css`
     .span-heading {
         font-family: var(--font-decorative);
-        font-size: 1.5rem;
+        font-size: 1.25rem;
         letter-spacing: 0.1em;
     }
 `
@@ -14,23 +14,38 @@ const styles = {
         justify-content: space-around;
         align-items: center;
         height: calc(100vh - 8rem);
-        padding-top: 6rem;
+        padding-top: 7rem;
         color: var(--font-light);
         ${spanHeading}
     `,
     SkillContainer: styled.div`
-        height: 80%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
     `,
     Certificates: styled.div`
         position: relative;
         .certificate-heading {
-            display: flex;
             position: absolute;
-            top: -3rem;
-            left: -1rem;
+            top: -2rem;
+            left: -3rem;
+            transform: rotate(-25deg);
+        }
+        .certificate-heading span {
+            display: block;
+        }
+        .certificate-heading img {
+            position: absolute;
+            transform: rotate(25deg);
+            width: 3rem;
+            height: 3rem;
+            left: 8.5rem;
+            top: 1rem;
         }
         img {
-            width: 16rem;
+            width: 15rem;
             height: 10rem;
         }
 
@@ -46,11 +61,61 @@ const styles = {
             height: 2.5rem;
             text-align: center;
             border-radius: 50%;
-            margin: 0.2em;
+            margin: 0 0.5em;
             background-color: var(--black);
             box-shadow: 0 0 10px var(--primary);
         }
     `,
+
+    SkillLists : styled.div`
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: red;
+        margin-top: 8rem;
+        button {
+            height: 7rem;
+            width: 7rem;
+            border-radius: 50%;
+            color: var(--font-light);
+            background-color: var(--black);
+            font-family: var(--font-secondary);
+            text-transform: uppercase;
+            border: none;
+            box-shadow: 0 0 25px var(--primary);
+        }
+        button span {
+            font-size: 1.75rem;
+        }
+
+        button:hover {
+            transform: scale(0.6);
+        }
+
+        .tech-skill {
+            position: absolute;
+            width: 20rem;
+            height: 20rem;
+            top: -7rem;
+            text-align: center;
+            opacity: 1;
+            background-color: #80808060;
+        }
+
+        .tech-skill *[class^="skill-"] {
+            background-color: red;
+            padding: 1rem;
+            position: absolute;
+            left: 0;
+            transform-origin: 100px;
+        }
+
+        button:hover ~ .tech-skill {
+            opacity: 1;
+        }
+    `,
+
     ImageContainer: styled.div`
         img {
             width: 18rem;
